747f4735e6f5d2c0b9c7b95186d9323649de299d
ladyrick <ladyrick@qq.com>
2019-10-14 16:59:58 +0800

    .

--- tools/ssh_patch.sh
+++ tools/ssh_patch.sh
@@ -3,7 +3,8 @@ function __oh_my_portable_ssh() {
 		/usr/bin/env ssh
 	else
 		local EOF_MARK=OH_MY_PORTABLE_EOF
-		/usr/bin/env ssh "$@" -t "bash --rcfile <(cat "'/etc/profile;'"
+		/usr/bin/env ssh "$@" -t "bash --rcfile <(cat '/etc/profile'
+([ -r "'~/.bash_profile'" ] && cat "'~/.bash_profile'") || ([ -r "'~/.bash_login'" ] && cat "'~/.bash_login'") || ([ -r "'~/.profile'" ] && cat "'~/.profile'")
 cat << '$EOF_MARK'
 $(cat $OH_MY_PORTABLE/dist/inline_profile.sh)
 $EOF_MARK
