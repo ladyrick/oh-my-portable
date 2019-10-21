7aef66c0988f0ec05259e81d705338cdd390ddd1
ladyrick <ladyrick@qq.com>
2019-10-21 16:52:18 +0800

    remove tab complete

--- rc.d/bashrc.d/03-bind.sh
+++ rc.d/bashrc.d/03-bind.sh
@@ -3,6 +3,5 @@ if [[ $- == *i* ]]; then
 	bind '"\e[B": history-search-forward'
 	bind '"\e[C": forward-char'
 	bind '"\e[D": backward-char'
-	bind '"\t": menu-complete'
 	bind '"\e[Z": menu-complete-backward'
 fi
