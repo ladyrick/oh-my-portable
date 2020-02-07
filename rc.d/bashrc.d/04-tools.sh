20d19119245616b1ca869f80c632fdc86a1b128c
ladyrick <ladyrick@qq.com>
2020-02-08 04:00:00 +0800

    .

--- rc.d/bashrc.d/04-tools.sh
+++ rc.d/bashrc.d/04-tools.sh
@@ -12,3 +12,68 @@ function keygen() {
     key=$(openssl rand -hex $(((keylen + 1) / 2)))
     echo ${key:0:${keylen}}
 }
+
+function __echo_color_8() {
+    while [[ -n "$1" ]]; do
+        if [[ "$1" == "-b" ]]; then
+            local decoration="1"
+            shift
+        elif [[ "$1" == "-u" ]]; then
+            local decoration="4"
+            shift
+        elif [[ "$1" == "-ub" || "$1" == "-bu" ]]; then
+            local decoration="1;4"
+            shift
+        elif ((30 <= $1 && $1 <= 37)); then
+            local color_code="$1"
+            shift
+        else
+            break
+        fi
+    done
+    if [[ -t 1 ]]; then
+        [[ -n "$decoration" ]] && echo -ne "\e[${decoration}m"
+        [[ -n "$color_code" ]] && echo -ne "\e[${color_code}m"
+        echo "$@"
+        echo -ne "\e[0m"
+    else
+        echo "$@"
+    fi
+}
+
+alias black="__echo_color_8 30"
+alias red="__echo_color_8 31"
+alias green="__echo_color_8 32"
+alias yellow="__echo_color_8 33"
+alias blue="__echo_color_8 34"
+alias magenta="__echo_color_8 35"
+alias cyan="__echo_color_8 36"
+alias white="__echo_color_8 37"
+
+function c256() {
+    while [[ -n "$1" ]]; do
+        if [[ "$1" == "-b" ]]; then
+            local decoration="1"
+            shift
+        elif [[ "$1" == "-u" ]]; then
+            local decoration="4"
+            shift
+        elif [[ "$1" == "-ub" || "$1" == "-bu" ]]; then
+            local decoration="1;4"
+            shift
+        elif [[ "$1" == 0 ]] || ((1 <= $1 && $1 <= 256)); then
+            local color_code="$1"
+            shift
+        else
+            break
+        fi
+    done
+    if [[ -t 1 ]]; then
+        [[ -n "$decoration" ]] && echo -ne "\e[${decoration}m"
+        [[ -n "$color_code" ]] && echo -ne "\e[38;5;${color_code}m"
+        echo "$@"
+        echo -ne "\e[0m"
+    else
+        echo "$@"
+    fi
+}
