6a299c6905ba76e08b8fac3c1903f2206adf720b
ladyrick <ladyrick@qq.com>
2020-05-30 14:08:12 +0800

    no need to get config when source

--- oh-my-portable.sh
+++ oh-my-portable.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 OH_MY_PORTABLE="$(cd "$(dirname "$BASH_SOURCE[0]")" && pwd)"
-source $OH_MY_PORTABLE/config.sh
-OH_MY_PORTABLE_CONFIG=
-[[ "$__only_patch_ssh" == "true" ]] && OH_MY_PORTABLE_CONFIG=o$OH_MY_PORTABLE_CONFIG
-[[ "$__portable_vim" == "true" ]] && OH_MY_PORTABLE_CONFIG=v$OH_MY_PORTABLE_CONFIG
-[[ "$__portable_git" == "true" ]] && OH_MY_PORTABLE_CONFIG=g$OH_MY_PORTABLE_CONFIG
-[[ "$__portable_bash" == "true" ]] && OH_MY_PORTABLE_CONFIG=b$OH_MY_PORTABLE_CONFIG
-[[ "$__portable_script" == "true" ]] && OH_MY_PORTABLE_CONFIG=s$OH_MY_PORTABLE_CONFIG
 
 if [[ "${BASH_SOURCE[0]}" == "$0" ]]; then
+	source $OH_MY_PORTABLE/config.sh
+	OH_MY_PORTABLE_CONFIG=
+	[[ "$__only_patch_ssh" == "true" ]] && OH_MY_PORTABLE_CONFIG=o$OH_MY_PORTABLE_CONFIG
+	[[ "$__portable_vim" == "true" ]] && OH_MY_PORTABLE_CONFIG=v$OH_MY_PORTABLE_CONFIG
+	[[ "$__portable_git" == "true" ]] && OH_MY_PORTABLE_CONFIG=g$OH_MY_PORTABLE_CONFIG
+	[[ "$__portable_bash" == "true" ]] && OH_MY_PORTABLE_CONFIG=b$OH_MY_PORTABLE_CONFIG
+	[[ "$__portable_script" == "true" ]] && OH_MY_PORTABLE_CONFIG=s$OH_MY_PORTABLE_CONFIG
 	source $OH_MY_PORTABLE/tools/compile.sh
 	source $OH_MY_PORTABLE/tools/install.sh
 else
