61bae32a36685847a8de80a45010e2795d38bd3f
ladyrick <ladyrick@qq.com>
2022-03-01 20:17:16 +0800

    add code wrapper

--- rc.d/bashrc.d/04-tools.sh
+++ rc.d/bashrc.d/04-tools.sh
@@ -104,3 +104,17 @@ function c256() {
 function hostip() {
 	hostname -I | cut -d ' ' -f 1
 }
+
+if [[ "$TERM_PROGRAM" == "vscode" ]]; then
+	function code() {
+		__bin_dir="$(dirname "$VSCODE_GIT_ASKPASS_NODE")/bin"
+		if [ -f "$__bin_dir/code" ]; then
+			"$__bin_dir/code" "$@"
+		elif [ -f "$__bin_dir/remote-cli/code" ]; then
+			"$__bin_dir/remote-cli/code" "$@"
+		else
+			echo '"code" script not found'
+			return 127
+		fi
+	}
+fi
