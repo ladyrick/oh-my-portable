62db5eadfbaa5f7065ae8a230992ab8697e173c5
ladyrick <ladyrick@qq.com>
2022-06-29 20:19:57 +0800

    pass return code to next command in PS1

--- rc.d/bashrc.d/00-PS1.sh
+++ rc.d/bashrc.d/00-PS1.sh
@@ -1,6 +1,11 @@
-function __prompt_color() {
-	# change prompt color according to $?
-	[[ "$?" == "0" ]] && echo -e '\033[36;1m' || echo -e '\033[31;1m'
+function __set_ret_code() {
+	"${@:2}"
+	return "$1"
 }
 
-PS1='\[\033[36;1m\]<\[\033[31;1m\]\h\[\033[36;1m\]> \[\033[35;1m\]\W\[$(__prompt_color)\] \$ \[\033[00m\]'
+function __prompt_color() {
+	# change prompt color according to $?
+	[[ "$?" == "0" ]] && echo -ne '\033[36;1m' || echo -ne '\033[31;1m'
+}
+
+PS1='\[\033[36;1m\]<\[\033[31;1m\]\h\[\033[36;1m\]> \[\033[35;1m\]\W\[$(__set_ret_code "$?" __prompt_color)\] \$ \[\033[0m\]'
